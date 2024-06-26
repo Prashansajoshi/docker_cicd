3ddb801ed89dd8f0e4a0f350270562b5c3343ba9
prashansa <joshiprashansa11@gmail.com>
2024-06-26 16:23:43 +0545

    terraform apply

---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+}
+
+variable "instance_type" {
+}
+
+# variable "key_name" {
+# }
+
+variable "security_group_id" {
+}
+
+variable "public_subnet_1" {
+}
+
+variable "aws_iam_instance_profile"{
+}
